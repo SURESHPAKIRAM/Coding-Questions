8d0c5f09d1850eca4099abfc12cd116f7bccb7db
SURESHPAKIRAM <pakiramsuresh@gmail.com>
2026-05-16 12:56:23 +0530

    changes in the file

--- Revese.java
+++ Revese.java
@@ -1,13 +1,29 @@
 import java.util.*;
 class Reverse{
+    public static void reverse(int [] arr,int s,int e){
+        while(s<e){
+            int temp=arr[s];
+            arr[s]=arr[e];
+            arr[e]=temp;
+            s++;
+            e--;
+        }
+
+    }
+    public static void rotate(int [] arr,int k){
+        k=k%arr.length;
+        if(k<0){
+            k=k+arr.length;
+        }
+        reverse(arr,0,k-1);
+        reverse(arr,k,arr.length-1);
+        reverse(arr,0,arr.length-1);
+
+    }
     public static void main(String[] args){
         int [] arr={3,2,5,6,7,1};
-        int res[]=new int[arr.length];
-        for(int i=arr.length-1;i>=0;i--){
-            res[arr.length-i-1]=arr[i];
-            
-        }
         System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(res));
+        rotate(arr,2);
+        System.out.println(Arrays.toString(arr));
     }
 }
